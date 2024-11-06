98c1b891de514521dc2fa614a53236ac4ba44d14
Kristopher Horton <khorton@miracosta.edu>
2024-11-06 08:30:20 -0800

    Screen Layouts

--- src/main/java/cs112/lab09/controllers/CityController.java
+++ src/main/java/cs112/lab09/controllers/CityController.java
@@ -1,4 +1,14 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.Constants;
+import cs112.lab09.models.RevisedHistoricalEvent;
+
+import static cs112.lab09.Constants.HISTORICAL_DATA;
+
 public class CityController {
+
+    public void initData(Constants.Event eventIndex) {
+        String[] data = HISTORICAL_DATA[eventIndex.ordinal()];
+        RevisedHistoricalEvent event = new RevisedHistoricalEvent(data);
+    }
 }
--- src/main/java/cs112/lab09/controllers/MapController.java
+++ src/main/java/cs112/lab09/controllers/MapController.java
@@ -1,4 +1,25 @@
 package cs112.lab09.controllers;
 
+import cs112.lab09.Constants;
+import cs112.lab09.RedSummer;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import static cs112.lab09.Constants.MAP_VIEW_RESOURCE;
+import static cs112.lab09.Constants.MAP_VIEW_TITLE;
+
 public class MapController {
+
+    public void handleOpenPopup() {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(RedSummer.class.getResource(MAP_VIEW_RESOURCE));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle(MAP_VIEW_TITLE);
+        Constants.Event eventIndex = Constants.Event.BISBEE;
+        CityController cityController = fxmlLoader.getController();
+        cityController.initData(eventIndex);
+        stage.show();
+    }
 }
